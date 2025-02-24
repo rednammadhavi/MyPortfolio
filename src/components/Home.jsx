4d834eedb21fe134698c4d6c73a1aa1e39788438
Rednam-madhavi <rednammadhavi@gmail.com>
2025-02-24 18:01:58 +0530

    Picture Pending

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React from 'react';
 import { FileUser } from 'lucide-react';
 
 const Home = () => {
 
   const resume = () => {
-    window.open("/resume.pdf")
+    window.open("/resume.pdf", "_blank");
   };
 
   return (
-    <>
-      <div className="h-[90vh] pt-3 bg-cover bg-center bg-opacity-50 bg-[url('/body.avif')] flex w-full justify-center">
-        <div className="flex flex-col items-start justify-center h-full bg-opacity-50 pl-[10vw]">
-          <h3 className="text-white text-4xl font-light">Hello, I am</h3>
-          <h1 className="text-gray-400 text-6xl font-bold">NAGA MADHAVI REDNAM</h1>
-          <h3 className="text-white text-xl font-light">Fullstack Developer | UI & UX Designer</h3>
-          <button onClick={resume} className='bg-red-700 rounded-full p-2 px-3 my-3 font-semibold text-white flex gap-1 hover:bg-red-800 cursor-pointer'>
-            <FileUser />
-            Resume
-          </button>
+    <div className="bg-black h-[90vh] py-5 bg-cover bg-center bg-[url('/body.avif')] flex flex-col md:flex-row justify-center md:justify-between items-center px-6 md:px-[10vw]">
+
+      <div className="flex flex-col items-start justify-center h-full w-full bg-opacity-50 text-center md:text-left">
+        <div className="text-center md:text-left flex flex-col items-center md:items-start">
+          <h3 className="text-white text-2xl md:text-4xl font-light">Hello, I am</h3>
+          <h1 className="text-gray-500 text-4xl md:text-6xl font-bold">NAGA MADHAVI REDNAM</h1>
+          <h3 className="text-white text-lg md:text-xl font-light">Fullstack Developer | UI & UX Designer</h3>
         </div>
+
+
+        <button
+          onClick={resume}
+          className="bg-red-700 rounded-full p-2 px-4 my-3 font-semibold text-white flex items-center gap-2 transition-all duration-200 hover:bg-red-800 hover:scale-105"
+        >
+          <FileUser size={20} />
+          Resume
+        </button>
       </div>
 
-    </>
-  )
-}
+      {/* <div className="mt-6 md:mt-0">
+        <img
+          src="/img.jpg"
+          alt="Profile"
+          className="w-48 h-48 md:w-96 md:h-96 rounded-full object-cover shadow-lg"
+        />
+      </div> */}
 
-export default Home
+    </div>
+  );
+};
+
+export default Home;
