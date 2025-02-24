d1d1bcfe27f29f7ac93a300bdb6619b8f1c67039
Rednam-madhavi <rednammadhavi@gmail.com>
2025-02-24 21:46:39 +0530

    Skills Completed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,34 +2,35 @@ import React from 'react';
 
 const Skills = () => {
   return (
-    <div className="bg-black text-white w-full p-6 flex flex-col items-center">
-      <h1 className="text-4xl font-bold text-center mb-6">Skills</h1>
+    <div className="bg-white text-black w-full p-6 flex flex-col items-center">
+      <h1 className="text-4xl font-semibold text-center mb-4 text-gray-900">Skills</h1>
+      <div className=" bg-gray-100  shadow-lg rounded-2xl p-6 w-full max-w-3xl">
+        <div className="flex flex-col gap-3">
+          <h1 className="font-semibold text-sm text-pink-800 py-1">
+            Programming Languages:
+            <span className="text-gray-900"> Python | Java | C | HTML | CSS | JavaScript</span>
+          </h1>
 
-      <div className="w-full max-w-5xl flex flex-col gap-3 p-2">
-        <h1 className="font-semibold text-sm py-1">
-          Programming Languages :  
-          <span className="font-light"> Python | Java | C | HTML | CSS | JavaScript</span>
-        </h1>
+          <h1 className="font-semibold text-sm text-pink-800 py-1">
+            Libraries/Frameworks:
+            <span className="text-gray-900"> jQuery | React | Node | Express | Next.js</span>
+          </h1>
 
-        <h1 className="font-semibold text-sm py-1">
-          Libraries/Frameworks :  
-          <span className="font-light"> jQuery | React | Node | Express | Next.js</span>
-        </h1>
+          <h1 className="font-semibold text-sm text-pink-800 py-1">
+            Tools/Platforms:
+            <span className="text-gray-900"> Git | GitHub | VS Code</span>
+          </h1>
 
-        <h1 className="font-semibold text-sm py-1">
-          Tools / Platforms :  
-          <span className="font-light"> Git | GitHub | VS Code</span>
-        </h1>
+          <h1 className="font-semibold text-sm text-pink-800 py-1">
+            Databases:
+            <span className="text-gray-900"> MySQL | MongoDB</span>
+          </h1>
 
-        <h1 className="font-semibold text-sm py-1">
-          Databases :  
-          <span className="font-light"> MySQL | MongoDB</span>
-        </h1>
-
-        <h1 className="font-semibold text-sm py-1">
-          Soft Skills :  
-          <span className="font-light"> Effective Communication | Adaptability | Critical Thinking | Team Collaboration | Self-Motivation</span>
-        </h1>
+          <h1 className="font-semibold text-sm text-pink-800 py-1">
+            Soft Skills:
+            <span className="text-gray-900"> Effective Communication | Adaptability | Critical Thinking | Team Collaboration | Self-Motivation</span>
+          </h1>
+        </div>
       </div>
     </div>
   );
