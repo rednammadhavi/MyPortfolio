06cf9b7900239f4a1108177a34bc33ffd46e609a
Rednam-madhavi <rednammadhavi@gmail.com>
2025-06-13 13:48:53 +0530

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,14 +2,14 @@ import React, { useRef, useState, useEffect } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const projects = [
-  {
-    name: "Textify",
-    image: "/assets/projects/textify.png",
-    description:
-      "Textify is a simple text editing app built with React and styled using Bootstrap. It lets users quickly change text case, count words and characters, remove extra spaces, and copy text with one click. The app also includes a dark mode for better readability.",
-    tags: ["React", "Bootstrap", "Text Utility"],
-    more: "https://github.com/Rednam-madhavi/Textify-SimpleTextUtilsApp",
-  },
+  // {
+  //   name: "Textify",
+  //   image: "/assets/projects/textify.png",
+  //   description:
+  //     "Textify is a simple text editing app built with React and styled using Bootstrap. It lets users quickly change text case, count words and characters, remove extra spaces, and copy text with one click. The app also includes a dark mode for better readability.",
+  //   tags: ["React", "Bootstrap", "Text Utility"],
+  //   more: "https://github.com/Rednam-madhavi/Textify-SimpleTextUtilsApp",
+  // },
   {
     name: "EchoNews",
     image: "/assets/projects/echonews.png",
@@ -18,22 +18,22 @@ const projects = [
     tags: ["React", "Bootstrap", "API Integration", "Real-time News Aggregation"],
     more: "https://github.com/Rednam-madhavi/EchoNews-NewsApplication",
   },
-  {
-    name: "TaskManager",
-    image: "/assets/projects/taskmanager.png",
-    description:
-      "TaskManager is a lightweight to-do list app built with Vite, React, and Tailwind CSS. It supports add, edit, delete operations with local storage. Perfect for staying organized across devices.",
-    tags: ["React", "Tailwind CSS", "To-Do List", "Local Storage"],
-    more: "https://github.com/Rednam-madhavi/TaskManager-SimpleToDoApp",
-  },
-  {
-    name: "SafeOp",
-    image: "/assets/projects/safeop.png",
-    description:
-      "SafeOp is a secure password manager built with Vite, React, and Tailwind CSS. It uses browser’s local storage for safely storing and retrieving passwords with a responsive design.",
-    tags: ["React", "Tailwind CSS", "Password Manager", "Local Storage"],
-    more: "https://github.com/Rednam-madhavi/SafeOp-PasswordManager",
-  },
+  // {
+  //   name: "TaskManager",
+  //   image: "/assets/projects/taskmanager.png",
+  //   description:
+  //     "TaskManager is a lightweight to-do list app built with Vite, React, and Tailwind CSS. It supports add, edit, delete operations with local storage. Perfect for staying organized across devices.",
+  //   tags: ["React", "Tailwind CSS", "To-Do List", "Local Storage"],
+  //   more: "https://github.com/Rednam-madhavi/TaskManager-SimpleToDoApp",
+  // },
+  // {
+  //   name: "SafeOp",
+  //   image: "/assets/projects/safeop.png",
+  //   description:
+  //     "SafeOp is a secure password manager built with Vite, React, and Tailwind CSS. It uses browser’s local storage for safely storing and retrieving passwords with a responsive design.",
+  //   tags: ["React", "Tailwind CSS", "Password Manager", "Local Storage"],
+  //   more: "https://github.com/Rednam-madhavi/SafeOp-PasswordManager",
+  // },
   {
     name: "GenImg",
     image: "/assets/projects/genimg.png",
@@ -108,14 +108,14 @@ const Projects = () => {
                     className="w-full h-48 object-contain border border-gray-200 rounded-lg"
                     onClick={() => setModalImage(project.image)}
                   />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 group-hover:bg-opacity-30 transition">
+                  {/* <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 group-hover:bg-opacity-30 transition">
                     <button
                       onClick={() => setModalImage(project.image)}
                       className="opacity-0 group-hover:opacity-100 text-white bg-pink-600 px-4 py-2 rounded-lg transition hover:bg-pink-700"
                     >
                       View Fullscreen
                     </button>
-                  </div>
+                  </div> */}
                 </div>
                 <div className="p-4">
                   <h3 className="text-2xl font-semibold text-pink-700 mb-1">
